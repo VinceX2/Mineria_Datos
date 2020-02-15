856dcbbda6bcb868f5ac8cc46b1c4ba0e045b823
Eduardoitt <jesus.escobedo16@tectijuana.edu.mx>
2020-02-14 17:59:58 -0800

    create file and first instructions

---
+++ Practicas/Practica1/Ejercicio1.R
@@ -0,0 +1,11 @@
+##1. Initialize sample size
+muestra<-10000
+##2. Initialize counter
+c<-0
+
+##3. loop for(i in rnorm(size))
+##I don't know very well if that's how rnorm is declared
+for(i in rnorm(muestra, mean=0,sd=1)){
+  
+}
+
