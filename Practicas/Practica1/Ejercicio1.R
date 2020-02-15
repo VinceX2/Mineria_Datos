bbf9e67e689b1ae606ad8cf9c477545c0b15019b
VinceX2 <vince.geek404@gmail.com>
2020-02-14 23:49:59 -0800

    Changing the if condition

--- Practicas/Practica1/Ejercicio1.R
+++ Practicas/Practica1/Ejercicio1.R
@@ -6,7 +6,7 @@ c<-0
 ##3. loop for(i in rnorm(size))
 ##I don't know very well if that's how rnorm is declared
 for(i in rnorm(n, mean=0,sd=1)) {
-  if(i >= -1) {
+  if(i >= -1  && i <= 1) {
     c <- c + 1
   }
 }
