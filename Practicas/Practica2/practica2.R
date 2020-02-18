7c97c63447f5f2e302b197cf62ccaa8636e198d8
Eduardoitt <jesus.escobedo16@tectijuana.edu.mx>
2020-02-17 23:30:00 -0800

    10 functions of r

--- Practicas/Practica2/practica2.R
+++ Practicas/Practica2/practica2.R
@@ -5,3 +5,23 @@ a <- 100
 length(a)
 
 quantile(a)
+
+##10 functions
+
+ceiling(3.475) 
+floor(3.475)
+sqrt(9)
+x <- "abcdef"
+substr(x, 2, 4) 
+
+dbinom(0:5, 10, .5)
+
+dunif(10, min=0, max=1)
+
+punif(10, min=0, max=1)
+
+median(10,na.rm = FALSE)
+
+diff(45, lag=1)
+
+rep(1:3, 2)
