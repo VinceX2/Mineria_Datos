7f6d71d5fa232f2e08c81538f4e60db1a1d0be82
VinceX2 <vince.geek404@gmail.com>
2020-03-06 15:10:43 -0800

    Adding changes on practica 3

--- Unidad1/Practicas/Practica3/Ejercicio3.R
+++ Unidad1/Practicas/Practica3/Ejercicio3.R
@@ -1,8 +1,8 @@
 #Data
 revenue <- c(14574.49, 7606.46, 8611.41, 9175.41, 8058.65, 8105.44, 11496.28, 9766.09, 10305.32, 14379.96, 10713.97, 15433.50)
 expenses <- c(12051.82, 5695.07, 12319.20, 12089.72, 8658.57, 840.20, 3285.73, 5821.12, 6976.93, 16618.61, 10054.37, 3803.96)
-
-
+  
+#Calculate Profit As The Differences Between Revenue And Expenses
 profit <- revenue  - expenses
 profit
 
@@ -24,11 +24,11 @@ mean_pat <- mean (profit.after.tax)
 mean_pat
 
 #Find The Months With Above-Mean Profit After Tax
-good.months <- _ _ mean_pat
+good.months <- profit > mean_pat
 good.months
 
 #Bad Months Are The Opposite Of Good Months !
-bad.months <- _good.months
+bad.months <- profit < good.months
 bad.months
 
 #The Best Month Is Where Profit After Tax Was Equal To The Maximum
@@ -41,9 +41,9 @@ worst.month
 
 #Convert All Calculations To Units Of One Thousand Dollars
 revenue.1000 <- round(revenue / 1000, 0)
-expenses.1000 <- _(_ _ _, _)
-profit.1000 <- _(_ _ _, _)
-profit.after.tax.1000 <- _(_ _ _, _)
+expenses.1000 <- round(expenses /1000 , 0)
+profit.1000 <- round(profit/1000 , 0)
+profit.after.tax.1000 <- round(profit.after.tax/1000 , 0)
 
 #Print Results
 revenue.1000
@@ -71,3 +71,4 @@ M <- rbind(
 )
 
 #Print The Matrix
+M
