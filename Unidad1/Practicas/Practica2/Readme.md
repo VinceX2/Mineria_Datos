7c44e7250dfa3c18b5d45327db37033bd4416d3a
Eduardoitt <jesus.escobedo16@tectijuana.edu.mx>
2020-03-06 00:32:38 -0800

    adding reade and practice 4

---
+++ Unidad1/Practicas/Practica2/Readme.md
@@ -0,0 +1,55 @@
+# Practice 2
+
+## Functions in R
+
+n <- dnorm(x = 10000, mean = 0, sd = 1, log = FALSE)
+n
+
+a <- 100
+length(a)
+
+quantile(a)
+
+## 10 functions
+
+* Redondea los decimales hacia el numero siguiente
+```R
+ceiling(3.475) # 4
+```
+* Redondea el numero al numero su numero menor
+```R
+floor(3.475) # 3
+```
+* razi cuadrada 
+```R
+sqrt(9) # 3
+```
+* substrae de una variable los indices indicados
+```R
+x <- "abcdef"
+substr(x, 2, 4) # bcd
+```
+*  Distribucion binomial
+```R
+dbinom(0:5, 10, .5)
+```
+* Distribucion uniforma
+```R
+dunif(10, min=0, max=1)
+```
+* Crea variables uniformes aleatoriamente
+```R
+punif(10, min=0, max=1)
+```
+* mediana
+```R
+mean(10,na.rm = FALSE)
+```
+* lagged differences, with lag indicating which lag to use
+```R
+diff(45, lag=1)
+```
+* repite los nuemros del 1-3 2 veces
+```R
+rep(1:3, 2)
+```
--- Unidad1/Practicas/Practica4/Matrices_Data.R
+++ Unidad1/Practicas/Practica4/Matrices_Data.R
@@ -56,7 +56,7 @@ colnames(Point) <- Seasons
 rownames(Point) <- Players
 Point
 #
-
+########### second matrix ############
 #Free Throw Attempts
 KobeBryant_FTA <- c(819,768,742,564,541,583,451,626,21,241)
 JoeJohnson_FTA <- c(330,314,379,362,269,243,186,161,195,176)
@@ -70,5 +70,39 @@ DerrickRose_FTA <- c(205,205,205,250,338,555,239,0,32,187)
 DwayneWade_FTA <- c(803,535,467,771,702,652,297,425,258,370)
 #Matrix
 #
+FreeThrowAttempts
 # <put your code here>
+FreeThrowAttempts <- rbind(KobeBryant_FTA, JoeJohnson_FTA, LeBronJames_FTA, CarmeloAnthony_FTA, DwightHoward_FTA, ChrisBosh_FTA, ChrisPaul_FTA, KevinDurant_FTA, DerrickRose_FTA, DwayneWade_FTA)
+rm(KobeBryant_FTA, JoeJohnson_FTA, LeBronJames_FTA, CarmeloAnthony_FTA, DwightHoward_FTA, ChrisBosh_FTA, ChrisPaul_FTA, KevinDurant_FTA, DerrickRose_FTA, DwayneWade_FTA)
+colnames(FreeThrowAttempts) <- Seasons
+rownames(FreeThrowAttempts) <- Players
+FreeThrowAttempts
 #
+########### function ############
+myplot <- function(z, who=1:10) {
+  matplot(t(z[who,,drop=F]), type="b", pch=15:18, col=c(1:4,6), main="Basketball Players Analysis")
+  legend("bottomleft", inset=0.01, legend=Players[who], col=c(1:4,6), pch=15:18, horiz=F)
+}
+myplot(FreeThrows)
+myplot(FreeThrowAttempts)
+
+#Games 
+KobeBryant_G <- c(80,77,82,82,73,82,58,78,6,35)
+JoeJohnson_G <- c(82,57,82,79,76,72,60,72,79,80)
+LeBronJames_G <- c(79,78,75,81,76,79,62,76,77,69)
+CarmeloAnthony_G <- c(80,65,77,66,69,77,55,67,77,40)
+DwightHoward_G <- c(82,82,82,79,82,78,54,76,71,41)
+ChrisBosh_G <- c(70,69,67,77,70,77,57,74,79,44)
+ChrisPaul_G <- c(78,64,80,78,45,80,60,70,62,82)
+KevinDurant_G <- c(35,35,80,74,82,78,66,81,81,27)
+DerrickRose_G <- c(40,40,40,81,78,81,39,0,10,51)
+DwayneWade_G <- c(75,51,51,79,77,76,49,69,54,62)
+#Matrix
+Games <- rbind(KobeBryant_G, JoeJohnson_G, LeBronJames_G, CarmeloAnthony_G, DwightHoward_G, ChrisBosh_G, ChrisPaul_G, KevinDurant_G, DerrickRose_G, DwayneWade_G)
+rm(KobeBryant_G, JoeJohnson_G, CarmeloAnthony_G, DwightHoward_G, ChrisBosh_G, LeBronJames_G, ChrisPaul_G, DerrickRose_G, DwayneWade_G, KevinDurant_G)
+colnames(Games) <- Seasons
+rownames(Games) <- Players
+
+
+
+
