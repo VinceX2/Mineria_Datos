489d24e7638d380ebeec298b0c4371f6cd711bb3
VinceX2 <vince.geek404@gmail.com>
2020-03-05 16:47:59 -0800

    Adding more examples into Practica5 file

--- Unidad1/Practicas/Practica5/practica5.r
+++ Unidad1/Practicas/Practica5/practica5.r
@@ -9,9 +9,15 @@ stats[stats$Income.Group == "Lower middle income",]
 
 #Filter countries by Upper middle income
 stats[stats$Income.Group == "Upper middle income",]
+
+#Filter by countrie Malta
+stats[stats$Country.Name == "Malta",]
+
 #Filter by countrie Qatar
 stats[stats$Country.Name =="Qatar",]
+
 #Filter by countrie Netherlands
 stats[stats$Country.Name =="Netherlands",]
+
 #Filter by countrie Norway
 stats[stats$Country.Name =="Norway",]
