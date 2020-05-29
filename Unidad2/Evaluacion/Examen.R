ee70af68c01f9794b50f73e66948f79d9e70c52d
Eduardoitt <jesus.escobedo16@tectijuana.edu.mx>
2020-05-28 23:22:24 -0700

    Readme Quiz 2

--- Unidad2/Evaluacion/Examen.R
+++ Unidad2/Evaluacion/Examen.R
@@ -1,58 +1,60 @@
 getwd()
-#setwd("/home/eduardo/Escritorio/Tareas Mineria/Examen")
-setwd("/Users/VinceLAB/Documents/ITT /Ene - Jun 2020/Mineria/RepoGitHub/MineriaRepo/Unidad2/Evaluacion")
+setwd("/home/eduardo/Escritorio/Tareas Mineria/Examen")
+#setwd("/Users/VinceLAB/Documents/ITT /Ene - Jun 2020/Mineria/RepoGitHub/MineriaRepo/Unidad2/Evaluacion")
 getwd()
 
-
+# We load the svc
 movies <- read.csv("Project-Data.csv")
-#Indicamos que columnas son las que utilizaremos de nuestro dataset
+# We indicate which columns are the ones we use from our dataset
 movies <- movies [c(3,6,8,18)]
-# Dado que el directivo de resenas le interesa solo ciertos generos de peliculas filtramos nuestro dataset
+# Since the review manager is only interested in certain genres of movies we filter our dataset
 movies<-movies[movies$Genre=="action"|movies$Genre=="adventure"|movies$Genre=="animation"|movies$Genre=="comedy"|movies$Genre=="drama",]
 
-# Nos dice que los estidios que le interesa son los siguiente Buena Vista Studios, Fox, Paramount Picture, Sony, Universal y WB
-# Filtramos nuestro dataset
+# It tells us that the studios you are interested in are the following, Buena Vista Studios, Fox, Paramount Picture, Sony, Universal and WB
 movies<-movies[movies$Studio=="Buena Vista Studios" | movies$Studio=="Fox" | movies$Studio=="Paramount Pictures" 
                |movies$Studio=="Sony" |movies$Studio=="Universal" |movies$Studio=="WB",]
 
-# miramos los primeros datos de nuestro dataset
+# we look at the first data of our dataset
 head(movies)
-# Renombramos las columnas
+# Rename the columns
 colnames(movies) <- c("Genre", "Studio", "BudgetMillions", "GrossUS")
-#Miramos los primeros 6 registros 
+# We look at the first 6 records
 head(movies)
-# Miramos los ultimos 6 registros
+# We look at the last 6 records
 tail(movies)
-# Miramos la estructura de nuestros datos
+# We look at the structure of our data
 str(movies)
-# Miramos un resumen genreal de los datos
+# We look at a general summary of the data
 summary(movies)
 
-# importamos la libreria ggplot2 para graficar y utlizar estetica
+
+# we import the ggplot2 library to graph and use aesthetics
 library(ggplot2)
 
 #boxplots
 
-# Comenzamos a contruir nuestra grafica
+# We start to build our graph
 
-# A nuestra varaiable u le cargamos nuetro dataset movies
-# para la estetica decimos que en el eje x sera determinado por Genre
-# el eje y lo determina GrossUS
-# Cada color en la grafica lo sera determinado por Studio y Size por BudgetMIlllions
+
+# To our varaiable we load our dataset movies
+# for aesthetics we say that on the x axis it will be determined by Genre
+# the axis and is determined by GrossUS
+# Each color in the chart will be determined by Studio and Size by BudgetMIlllions
 u <- ggplot(movies, aes(x=Genre, y=GrossUS,
                         color=Studio, size=BudgetMillions))
 
-# Ploteamos indicando el tipo de grafica en este caso con diagramas de cajas
+
+# Plot indicating the type of graph in this case with box diagrams
 u + geom_boxplot()  
 
-# Indicamos el grosor del contorno y el color de las cajas agregando la dencidad de puntos en ella
+# We indicate the thickness of the outline and the color of the boxes by adding the density of points on it
 u +  geom_jitter() + geom_boxplot(size=0.3,color="Black") 
 
-# Indicamos la opacidad de las cajas para poder notar la dispercion quitando los puntos atipicos
-# finalmente le agregamos un titulo y nombramos nuestros ejes "x" y "y"
 
+# We indicate the opacity of the boxes to be able to notice the dispersion removing the unusual points
+# finally we add a title and name our axes "x" and "y"
 u + geom_jitter(shape=20) + geom_boxplot(size=0.3,alpha=0.5,color="Black",outlier.shape = NA)+ theme(
   plot.title = element_text(color="Black", size=14, face="bold", hjust = 0.5),
   axis.title.x = element_text(color="blue", size=14, face="bold"),
   axis.title.y = element_text(color="purple", size=14, face="bold")
-)  +  ggtitle("Domestic Gross % by Genre") + ylab("Gross%US") 
+)  +  ggtitle("Domestic Gross % by Genre") + ylab("Gross %US") 
